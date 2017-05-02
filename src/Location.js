10fd729250be54fc1c53994cf084cfb1e80accc0
Gabe Martin <gabema@gmail.com>
2017-05-02 15:40:46 -0700

    Updating to a stateless functional component

--- src/Location.js
+++ src/Location.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import {geolocated, geoPropTypes} from 'react-geolocated';
- 
-class Location extends React.Component {
-  render() {
-    return !this.props.isGeolocationAvailable
+
+// https://www.npmjs.com/package/react-geolocated
+const Location = ({coords, isGeolocationAvailable, isGeolocationEnabled}) => {
+    return !isGeolocationAvailable
       ? <div>Your browser does not support Geolocation</div>
-      : !this.props.isGeolocationEnabled
+      : !isGeolocationEnabled
         ? <div>Geolocation is not enabled</div>
-        : this.props.coords
+        : coords
           ? <table>
             <tbody>
-              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
-              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
-              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
-              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
-              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+              <tr><td>latitude</td><td>{coords.latitude}</td></tr>
+              <tr><td>longitude</td><td>{coords.longitude}</td></tr>
+              <tr><td>altitude</td><td>{coords.altitude}</td></tr>
+              <tr><td>heading</td><td>{coords.heading}</td></tr>
+              <tr><td>speed</td><td>{coords.speed}</td></tr>
             </tbody>
           </table>
           : <div>Getting the location data&hellip; </div>;
-  }
-}
+};
 
 Location.propTypes = {...Location.propTypes, ...geoPropTypes};
 
 export default geolocated({
   positionOptions: {
-    enableHighAccuracy: false,
+    enableHighAccuracy: true,
   },
   userDecisionTimeout: 5000
 })(Location);
